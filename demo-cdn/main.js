03555d5fe8f0def1685f85b5fd2e3e4adfd42875
Lukas Herman <lherman.cs@gmail.com>
2024-12-30 15:09:13 -0500

    init demo-cdn

---
+++ demo-cdn/main.js
@@ -0,0 +1,41 @@
+import handler from "serve-handler";
+import http from "http";
+import { App, FirewallClaims, PeerClaims } from "@pulsebeam/server/node";
+
+// default values are only used for testing only!!
+const appId = process.env["PULSEBEAM_APP_ID"] || "app_e66Jb4zkt66nvlUKMRTSZ";
+const appSecret = process.env["PULSEBEAM_APP_SECRET"] ||
+  "sk_7317736f8a8d075a03cdea6b6b76094ae424cbf619a8e9273e633daed3f55c38";
+const app = new App(appId, appSecret);
+
+const server = http.createServer((request, response) => {
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  if (url.pathname === "/auth") {
+    const groupId = url.searchParams.get("groupId");
+    const peerId = url.searchParams.get("peerId");
+
+    if (!groupId || !peerId) {
+      response.writeHead(400, { "Content-Type": "text/plain" });
+      response.end("Bad Request - groupId and peerId are required");
+      return;
+    }
+
+    const claims = new PeerClaims(groupId, peerId);
+    const rule = new FirewallClaims("*", "*");
+    claims.setAllowIncoming0(rule);
+    claims.setAllowOutgoing0(rule);
+
+    const token = app.createToken(claims, 3600);
+    response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    response.end(token);
+    return;
+  }
+
+  // You pass two more arguments for config and middleware
+  // More details here: https://github.com/vercel/serve-handler#options
+  return handler(request, response);
+});
+
+server.listen(3000, () => {
+  console.log("Running at http://localhost:3000");
+});
