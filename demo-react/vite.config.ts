f1e6b34875a041a4f5cea1ec5605028249b0d97c
Lukas Herman <lherman.cs@gmail.com>
2025-01-17 10:40:00 -0500

    update demo-react to latest server

--- demo-react/vite.config.ts
+++ demo-react/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { App, FirewallClaims, PeerClaims } from "@pulsebeam/server/node";
+import { AccessToken, PeerClaims, PeerPolicy } from "@pulsebeam/server/node";
 
 // default values are only used for testing only!!
-const appId = process.env["PULSEBEAM_APP_ID"] || "app_e66Jb4zkt66nvlUKMRTSZ";
+const appId = process.env["PULSEBEAM_APP_ID"] || "kid_bc74ea55b2ffe97c";
 const appSecret = process.env["PULSEBEAM_APP_SECRET"] ||
-  "sk_7317736f8a8d075a03cdea6b6b76094ae424cbf619a8e9273e633daed3f55c38";
-const app = new App(appId, appSecret);
+  "sk_360e45d1d7cb3ea840789f56f6502b4154f22aa89b67b557fa9427363968ffe4";
+const app = new AccessToken(appId, appSecret);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -34,9 +34,9 @@ export default defineConfig({
             }
 
             const claims = new PeerClaims(groupId, peerId);
-            const rule = new FirewallClaims("*", "*");
-            claims.setAllowIncoming0(rule);
-            claims.setAllowOutgoing0(rule);
+            const rule = new PeerPolicy("*", "*");
+            claims.setAllowPolicy(rule);
+            claims.setAllowPolicy(rule);
 
             const token = app.createToken(claims, 3600);
             res.writeHead(200, {
