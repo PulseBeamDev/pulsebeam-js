708d8b1872c107c21ca2ae7a3d3f45300ad7d989
Lukas Herman <lherman.cs@gmail.com>
2025-01-17 15:51:42 -0500

    rename env vars

--- demo-react/vite.config.ts
+++ demo-react/vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import { AccessToken, PeerClaims, PeerPolicy } from "@pulsebeam/server/node";
 
 // default values are only used for testing only!!
-const appId = process.env["PULSEBEAM_APP_ID"] || "kid_bc74ea55b2ffe97c";
-const appSecret = process.env["PULSEBEAM_APP_SECRET"] ||
+const appId = process.env["PULSEBEAM_API_KEY"] || "kid_bc74ea55b2ffe97c";
+const appSecret = process.env["PULSEBEAM_API_SECRET"] ||
   "sk_360e45d1d7cb3ea840789f56f6502b4154f22aa89b67b557fa9427363968ffe4";
 const app = new AccessToken(appId, appSecret);
 
