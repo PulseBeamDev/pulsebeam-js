39d753731dc3fd7e99fec034b90f7541342920b8
Lukas Herman <lherman.cs@gmail.com>
2025-01-21 19:18:28 -0500

    use conditional build for prod

--- demo-react/vite.config.ts
+++ demo-react/vite.config.ts
@@ -1,37 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { auth } from "./functions/auth.ts";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // "/auth": {
-      //   target: "http://localhost:8080",
-      //   changeOrigin: true,
-      //   secure: false,
-      // },
-      "/auth": {
-        // https://github.com/angular/angular-cli/issues/26198
-        target: "http://127.0.0.1:5173",
-        bypass(req, res) {
-          // URL requires a base url...
-          const url = new URL(req.url!, `http://${req.headers.host}`);
-          if (url.pathname === "/auth") {
-            try {
-              const token = auth(url);
-              res.writeHead(200, {
-                "Content-Type": "text/plain; charset=utf-8",
-              });
-              res.end(token);
-            } catch (e) {
-              res.writeHead(400, { "Content-Type": "text/plain" });
-              res.end("Bad Request - groupId and peerId are required");
-            }
-          }
-        },
-      },
+export default defineConfig(async ({ mode }) => {
+  const config: UserConfig = {
+    plugins: [react()],
+    server: {
+      proxy: {},
     },
-  },
+  };
+
+  if (mode !== "production") {
+    const { auth } = await import("./functions/auth.ts");
+    config.server!.proxy!["/auth"] = {
+      // https://github.com/angular/angular-cli/issues/26198
+      target: "http://127.0.0.1:5173",
+      bypass(req, res) {
+        // URL requires a base url...
+        const url = new URL(req.url!, `http://${req.headers.host}`);
+        if (url.pathname === "/auth") {
+          try {
+            const token = auth(url);
+            res.writeHead(200, {
+              "Content-Type": "text/plain; charset=utf-8",
+            });
+            res.end(token);
+          } catch (e) {
+            res.writeHead(400, { "Content-Type": "text/plain" });
+            res.end("Bad Request - groupId and peerId are required");
+          }
+        }
+      },
+    };
+  }
+
+  return config;
 });
