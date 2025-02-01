32f6206cecdfcf6a97d5d7c6226b77cbfd224656
Lukas Herman <lherman.cs@gmail.com>
2025-02-01 18:18:23 -0500

    feat: use native CF functions

--- demo-react/functions/auth.ts
+++ demo-react/functions/auth.ts
@@ -1,14 +1,12 @@
 import { AccessToken, PeerClaims, PeerPolicy } from "@pulsebeam/server/workerd";
 
-// default values are only used for testing only!!
-const appId = process.env["PULSEBEAM_API_KEY"] || "kid_bc74ea55b2ffe97c";
-const appSecret = process.env["PULSEBEAM_API_SECRET"] ||
-  "sk_360e45d1d7cb3ea840789f56f6502b4154f22aa89b67b557fa9427363968ffe4";
-const app = new AccessToken(appId, appSecret);
+interface Env {
+  PULSEBEAM_API_KEY: string;
+  PULSEBEAM_API_SECRET: string;
+}
 
-export function auth(
-  url: URL,
-): string {
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const url = new URL(context.request.url);
   const groupId = url.searchParams.get("groupId");
   const peerId = url.searchParams.get("peerId");
 
@@ -21,12 +19,10 @@ export function auth(
   claims.setAllowPolicy(rule);
   claims.setAllowPolicy(rule);
 
+  const app = new AccessToken(
+    context.env.PULSEBEAM_API_KEY,
+    context.env.PULSEBEAM_API_SECRET,
+  );
   const token = app.createToken(claims, 3600);
-  return token;
-}
-
-export const onRequest: PagesFunction<{}> = async (context) => {
-  const url = new URL(context.request.url);
-  const token = auth(url);
   return new Response(token);
 };
