f384d38dbbf4f40835b18aeb0252b77a34d1df32
Gabrielle Herman <33128928+gherman-cs@users.noreply.github.com>
2025-02-15 13:25:00 -0500

    improve tests

--- adapter-peerjs/test/adapter.spec.ts
+++ adapter-peerjs/test/adapter.spec.ts
@@ -1,8 +1,7 @@
 import { describe, it, expect } from "vitest";
-import { GROUP_ID, Peer, PulseBeamOptions, } from "../src/adapter";
+import { GROUP_ID, Peer, DataConnection } from "../src/adapter";
 
-async function getToken(){
-  const peerId = "peerjs-adapter-peer1"
+async function getToken(peerId: string){
   // See https://pulsebeam.dev/docs/guides/token/#example-nodejs-http-server
   // For explanation of this token-serving method
   const resp = await fetch(
@@ -12,76 +11,263 @@ async function getToken(){
   return token
 }
 
-async function exampleUsage() {
-  const token = await getToken();
-  const pulsebeamOptions: PulseBeamOptions = {
-    pulsebeam:{
-      token, 
-    },
-    debug: 3, // Enable debug logging level 3
-  };
+// async function exampleUsage() {
+//   const peerId = "peerjs-adapter-peer1"
+//   const token = await getToken(peerId);
+//   const pulsebeamOptions: PulseBeamOptions = {
+//     pulsebeam:{
+//       token, 
+//     },
+//     debug: 3, // Enable debug logging level 3
+//   };
 
-  const peer = new Peer(undefined, pulsebeamOptions);
+//   const peer = new Peer(undefined, pulsebeamOptions);
 
-  peer.on('open', (id) => {
-      console.log('PeerJS Peer opened with ID:', id);
+//   peer.on('open', (id) => {
+//       console.log('PeerJS Peer opened with ID:', id);
 
-      const connectToPeerId = 'peerjs-adapter-peer2'; // Replace with the peerId you want to connect to in the same group
-      const dc = peer.connect(connectToPeerId);
+//       const connectToPeerId = 'peerjs-adapter-peer2'; // Replace with the peerId you want to connect to in the same group
+//       const dc = peer.connect(connectToPeerId);
 
-      dc.on('open', () => {
-          console.log('Data channel opened');
-          dc.send('Hello from PeerJS Adapter!');
-      });
+//       dc.on('open', () => {
+//           console.log('Data channel opened');
+//           dc.send('Hello from PeerJS Adapter!');
+//       });
 
-      dc.on('data', (data) => {
-          console.log('Data received:', data);
-      });
+//       dc.on('data', (data) => {
+//           console.log('Data received:', data);
+//       });
 
-      dc.on('close', () => {
-          console.log('Data channel closed');
-      });
+//       dc.on('close', () => {
+//           console.log('Data channel closed');
+//       });
 
-      dc.on('error', (err) => {
-          console.error('Data channel error:', err);
-      });
-  });
+//       dc.on('error', (err) => {
+//           console.error('Data channel error:', err);
+//       });
+//   });
 
-  peer.on('connection', (dataConnection: DataConnection) => {
-      console.log('Incoming connection from:', dataConnection.peer);
+//   peer.on('connection', (dataConnection: DataConnection) => {
+//       console.log('Incoming connection from:', dataConnection.peer);
 
-      dataConnection.on('data', (data) => {
-          console.log('Received data on incoming connection:', data);
-          dataConnection.send('Hello back from incoming!');
-      });
+//       dataConnection.on('data', (data) => {
+//           console.log('Received data on incoming connection:', data);
+//           dataConnection.send('Hello back from incoming!');
+//       });
 
-      dataConnection.on('open', () => {
-          console.log('Incoming data channel opened');
-      });
-      dataConnection.on('close', () => {
-          console.log('Incoming data channel closed');
-      });
+//       dataConnection.on('open', () => {
+//           console.log('Incoming data channel opened');
+//       });
+//       dataConnection.on('close', () => {
+//           console.log('Incoming data channel closed');
+//       });
 
-      dataConnection.on('error', (err) => {
-          console.error('Incoming data channel error:', err);
-      });
-  });
+//       dataConnection.on('error', (err) => {
+//           console.error('Incoming data channel error:', err);
+//       });
+//   });
 
 
-  peer.on('disconnected', () => {
-      console.log('Peer disconnected from server');
-  });
+//   peer.on('disconnected', () => {
+//       console.log('Peer disconnected from server');
+//   });
 
-  peer.on('close', () => {
-      console.log('Peer closed');
-  });
+//   peer.on('close', () => {
+//       console.log('Peer closed');
+//   });
 
-  peer.on('error', (err) => {
-      console.error('Peer error:', err);
-  });
+//   peer.on('error', (err) => {
+//       console.error('Peer error:', err);
+//   });
 
-  // To connect from another Peer instance (e.g., standard PulseBeam or another PeerJS adapter instance)
-  // You would use the 'peerjs-adapter-peer2' (or whatever peerId you set) as the target peerId
+//   // To connect from another Peer instance (e.g., standard PulseBeam or another PeerJS adapter instance)
+//   // You would use the 'peerjs-adapter-peer2' (or whatever peerId you set) as the target peerId
+// }
+const cuesend = { 'initalize': 
+  async function initialize(peerId: string) {
+    let conn: undefined | DataConnection = undefined;
+  const token = await getToken(peerId)
+    // Create own peer object with connection to shared PeerJS server
+    const peer = new Peer(undefined, {
+        debug: 2,
+        pulsebeam:{token}
+    });
+
+    peer.on('open', function (id) {
+        // Workaround for peer.reconnect deleting previous id
+        if (peer.id === null) {
+            console.log('Received null id from peer open');
+            peer.id = lastPeerId;
+        } else {
+            lastPeerId = peer.id;
+        }
+
+        console.log('ID: ' + peer.id);
+    });
+    peer.on('connection', function (c) {
+        // Disallow incoming connections
+        c.on('open', function() {
+            c.send("Sender does not accept incoming connections");
+            setTimeout(function() { c.close(); }, 500);
+        });
+    });
+    peer.on('disconnected', function () {
+        // status.innerHTML = "Connection lost. Please reconnect";
+        console.log('Connection lost. Please reconnect');
+
+        // Workaround for peer.reconnect deleting previous id
+        // peer.id = lastPeerId;
+        // peer._lastServerId = lastPeerId;
+        peer.reconnect();
+    });
+    peer.on('close', function() {
+        conn = undefined;
+        // status.innerHTML = "Connection destroyed. Please refresh";
+        console.log('Connection destroyed');
+    });
+    peer.on('error', function (err) {
+        console.log(err);
+        alert('' + err);
+    });
+},
+
+/**
+ * Create the connection between the two Peers.
+ *
+ * Sets up callbacks that handle any events related to the
+ * connection and data received on it.
+ */
+'join': function join(otherPeerId: string) {
+    // Close old connection
+    if (conn) {
+        conn.close();
+    }
+
+    // Create connection to destination peer specified in the input field
+    conn = peer.connect(otherPeerId, {
+        reliable: true
+    });
+
+    conn.on('open', function () {
+        // status.innerHTML = "Connected to: " + conn.peer;
+        console.log("Connected to: " + conn.peer);
+
+        // Check URL params for comamnds that should be sent immediately
+        // var command = getUrlParam("command");
+        // if (command)
+        //     conn.send(command);
+    });
+    // Handle incoming data (messages only since this is the signal sender)
+    conn.on('data', function (data) {
+        // addMessage("<span class=\"peerMsg\">Peer:</span> " + data);
+        console.log("<span class=\"peerMsg\">Peer:</span> " + data)
+    });
+    conn.on('close', function () {
+        // status.innerHTML = "Connection closed";
+    });
+}
+}
+const cuerecieve = { 'initalize':
+/**
+ * Create the Peer object for our end of the connection.
+ *
+ * Sets up callbacks that handle any events related to our
+ * peer object.
+ */
+async function initialize(peerId: string) {
+  let conn: DataConnection | undefined = undefined;
+  const token = await getToken(peerId)
+    // Create own peer object with connection to shared PeerJS server
+    const peer = new Peer(undefined, {
+        debug: 2,
+        pulsebeam:{token}
+    });
+
+    peer.on('open', function (id) {
+        // // Workaround for peer.reconnect deleting previous id
+        // if (peer.id === null) {
+        //     console.log('Received null id from peer open');
+        //     peer.id = lastPeerId;
+        // } else {
+        //     lastPeerId = peer.id;
+        // }
+
+        console.log('ID: ' + peer.id);
+        // recvId.innerHTML = "ID: " + peer.id;
+        // status.innerHTML = "Awaiting connection...";
+    });
+    peer.on('connection', function (c) {
+        // Allow only a single connection
+        if (conn && conn.open) {
+            c.on('open', function() {
+                c.send("Already connected to another client");
+                setTimeout(function() { c.close(); }, 500);
+            });
+            return;
+        }
+
+        // @ts-ignore
+        conn = c; // TODO: make both compatible! b/c we don't own peerjs types
+        console.log("Connected to: " + conn?.peer);
+        // status.innerHTML = "Connected";
+        // ready();
+    });
+    peer.on('disconnected', function () {
+        // status.innerHTML = "Connection lost. Please reconnect";
+        console.log('Connection lost. Please reconnect');
+
+        // Workaround for peer.reconnect deleting previous id
+        // peer.id = lastPeerId;
+        // peer._lastServerId = lastPeerId;
+        peer.reconnect();
+    });
+    peer.on('close', function() {
+        conn = undefined;
+        // status.innerHTML = "Connection destroyed. Please refresh";
+        console.log('Connection destroyed');
+    });
+    peer.on('error', function (err) {
+        console.log(err);
+        alert('' + err);
+    });
+},
+
+'ready': function ready() {
+    conn.on('data', function (data) {
+        console.log("Data recieved");
+        var cueString = "<span class=\"cueMsg\">Cue: </span>";
+        switch (data) {
+            case 'Go':
+                // go();
+                console.log(cueString+data)
+                // addMessage(cueString + data);
+                break;
+            case 'Fade':
+                // fade();
+                console.log(cueString+data)
+                // addMessage(cueString + data);
+                break;
+            case 'Off':
+                // off();
+                console.log(cueString+data)
+                // addMessage(cueString + data);
+                break;
+            case 'Reset':
+                // reset();
+                console.log(cueString+data)
+                // addMessage(cueString + data);
+                break;
+            default:
+                console.log("<span class=\"peerMsg\">Peer: </span>" + data)
+                // addMessage("<span class=\"peerMsg\">Peer: </span>" + data);
+                break;
+        };
+    });
+    conn.on('close', function () {
+        // status.innerHTML = "Connection reset<br>Awaiting connection...";
+        conn = null;
+    });
+}
 }
 
 describe("Adapter", () => {
@@ -90,7 +276,12 @@ describe("Adapter", () => {
     // Test your adapter's logic here
     // expect(adapter.isConnected()).toBe(true)
     // ;
-    exampleUsage();
+    // exampleUsage();
+
+    cuerecieve.initalize("cue-recieve")
+    cuesend.initalize("cue-send")
+    cuerecieve.ready()
+    cuesend.join("cue-recieve")
     expect(adapter).not.toBeNull()
   });
 });
