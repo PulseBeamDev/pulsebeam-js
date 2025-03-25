342a5ce3083c636268e28f4efaa14458c172935d
Lukas Herman <lherman.cs@gmail.com>
2025-03-25 14:19:05 -0400

    init portal functions

---
+++ portal/functions/auth.ts
@@ -0,0 +1,28 @@
+import { AccessToken, PeerClaims, PeerPolicy } from "@pulsebeam/server/workerd";
+
+interface Env {
+  PULSEBEAM_API_KEY: string;
+  PULSEBEAM_API_SECRET: string;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const url = new URL(context.request.url);
+  const groupId = url.searchParams.get("groupId");
+  const peerId = url.searchParams.get("peerId");
+
+  if (!groupId || !peerId) {
+    throw new Error("groupId and peerId are required");
+  }
+
+  const claims = new PeerClaims(groupId, peerId);
+  const rule = new PeerPolicy("*", "*");
+  claims.setAllowPolicy(rule);
+  claims.setAllowPolicy(rule);
+
+  const app = new AccessToken(
+    context.env.PULSEBEAM_API_KEY,
+    context.env.PULSEBEAM_API_SECRET,
+  );
+  const token = app.createToken(claims, 3600);
+  return new Response(token);
+};
