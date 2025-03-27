55d5faaf83f3b6237d7ecf80f21f685d6213eeca
Gabrielle Herman <33128928+gherman-cs@users.noreply.github.com>
2025-03-27 10:01:39 -0400

    Clean up & Update index.ts

--- demo-multiplayer-games/cssbattle/functions/src/index.ts
+++ demo-multiplayer-games/cssbattle/functions/src/index.ts
@@ -64,26 +64,4 @@ export const getToken = onRequest({ cors: true }, async (request, response) => {
         logger.error("Error", error);
         response.status(500).send({ error: "Internal server error" });
     }
-    // logger.info("Hello logs!", {structuredData: true});
-
-    // const url = new URL(context.request.url);
-    // const groupId = url.searchParams.get("groupId");
-    // const peerId = url.searchParams.get("peerId");
-
-    // if (!groupId || !peerId) {
-    //     throw new Error("groupId and peerId are required");
-    // }
-
-    // const claims = new PeerClaims(groupId, peerId);
-    // const rule = new PeerPolicy("*", "*");
-    // claims.setAllowPolicy(rule);
-    // claims.setAllowPolicy(rule);
-
-    // const app = new AccessToken(
-    //     context.env.PULSEBEAM_API_KEY,
-    //     context.env.PULSEBEAM_API_SECRET,
-    // );
-    // const token = app.createToken(claims, 3600);
-    // return new Response(token);
-    // response.send({"message": "Hello from Firebase!"});
 });
