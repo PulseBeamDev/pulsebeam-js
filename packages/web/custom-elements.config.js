3a605df92c1498e1d6ec00b14fe9982f4b29a737
Lukas Herman <lherman.cs@gmail.com>
2026-01-13 20:48:39 -0500

    output cem json to dist

--- packages/web/custom-elements.config.js
+++ packages/web/custom-elements.config.js
@@ -1,20 +1,20 @@
 export default {
-    /** Globs to analyze */
-    globs: ['src/components/*.ts'],
-    /** Globs to exclude */
-    exclude: ['src/components/index.ts'],
-    /** Directory to output the manifest to */
-    outdir: 'dist',
-    /** Whether to run in dev mode, default is false */
-    dev: false,
-    /** Whether to run in watch mode, default is false */
-    watch: false,
-    /** Whether to include third party custom elements manifests */
-    dependencies: true,
-    /** Output the manifest as a JSON file */
-    packageJson: true,
-    /** Package name */
-    packageName: '@pulsebeam/web',
-    /** Lit plugin */
-    litelement: true,
+  /** Globs to analyze */
+  globs: ['src/components/*.ts'],
+  /** Globs to exclude */
+  exclude: ['src/components/index.ts'],
+  /** Directory to output the manifest to */
+  outdir: 'dist',
+  /** Whether to run in dev mode, default is false */
+  dev: false,
+  /** Whether to run in watch mode, default is false */
+  watch: false,
+  /** Whether to include third party custom elements manifests */
+  dependencies: true,
+  /** Output the manifest as a JSON file */
+  packageJson: true,
+  /** Package name */
+  packageName: '@pulsebeam/web',
+  /** Lit plugin */
+  litelement: true,
 };
