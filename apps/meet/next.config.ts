88dd1061f2d206cf724f6fdf5cafca252b96e8a6
Lukas Herman <lherman.cs@gmail.com>
2026-02-12 05:55:50 -0500

    add cname config

--- apps/meet/next.config.ts
+++ apps/meet/next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   transpilePackages: ["@pulsebeam/ui", "@pulsebeam/react"],
   reactStrictMode: true,
-  output: "export"
+  output: "export",
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
